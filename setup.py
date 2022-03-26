cfae2f63ae31b9b7816af7ffca1db9136fa687e9
Matthew Massey <69476319+masseygeo@users.noreply.github.com>
2022-03-26 17:24:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,15 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 
+                'numpy', 
+                'pandas', 
+                'scipy',
+                'matplotlib',
+                'tkinter',
+                'openpyxl',
+                'os'
+               ]
 
 test_requirements = ['pytest>=3', ]
 
