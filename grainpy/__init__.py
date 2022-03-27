d0b21620593697de3ab447220d445617cdea1fa5
Matthew Massey <69476319+masseygeo@users.noreply.github.com>
2022-03-27 11:31:14 -0400

    Update __init__.py

--- grainpy/__init__.py
+++ grainpy/__init__.py
@@ -1,9 +1,1 @@
-"""Top-level package for GrainPy."""
 
-__author__ = """Matthew Massey"""
-__email__ = 'mamass1@g.uky.edu'
-__version__ = '0.1.0'
-
-from .grainpy import *
-from .grainclass import *
-from .util import *
