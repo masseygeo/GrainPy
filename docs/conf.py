ab8f37d12b119d11803dfda2949f611fe09bd1ac
Matthew Massey <69476319+masseygeo@users.noreply.github.com>
2022-03-29 20:20:06 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = 'v0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'myst-parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
